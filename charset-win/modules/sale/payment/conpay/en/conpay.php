9980f44d3f247ba5b7ff082407dce7bd5b3bcbc9
leamix <x8p@leamix.com>
2013-02-20 18:48:52 +0400

    added second folder with win charset

--- charset-win/modules/sale/payment/conpay/en/conpay.php
+++
@@ -1,7 +0,0 @@
-<?
-global $MESS;
-$MESS["CONPAY_FILL_IN"] = "Fill in a form";
-$MESS["SALE_CONPAY_DESCRIPTION"] = "<a href=\"http://www.conpay.ru\" target=\"_blank\">http://www.conpay.ru</a>";
-$MESS['SALE_CONPAY_BUTTON_TEXT'] = 'Button content';
-$MESS['SALE_CONPAY_BUTTON_TEXT_DESC'] = 'HTML-markup inside a button container';
-?>
