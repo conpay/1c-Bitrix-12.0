21ef2265e29c1aae11c622976e186dfc2ef36110
leamix <x8p@leamix.com>
2013-02-20 16:41:10 +0400

    charset fix

--- modules/conpay/lang/ru/install/index.php
+++ modules/conpay/lang/ru/install/index.php
@@ -1,10 +1,8 @@
 <?php
-
 $MESS["CONPAY_MODULE_NAME"] = "Сервис CONPAY.RU";
 $MESS["CONPAY_MODULE_DESCRIPTION"] = 'Модуль позволяет разместить кнопку "Купить в кредит" в Вашем интернет-магазине';
 $MESS["CONPAY_PAYMENT_SYSTEM_NAME"] = "Купить в кредит, CONPAY.RU";
 $MESS["CONPAY_PAYMENT_SYSTEM_DESCRIPTION"] = 'Платежный модуль CONPAY.RU';
 $MESS['SALE_CONPAY_BUTTON_TEXT'] = 'Содержание кнопки';
 $MESS['SALE_CONPAY_BUTTON_TEXT_DESC'] = 'HTML-код внутри контейнера кнопки';
-
 ?>
