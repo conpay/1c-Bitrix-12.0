2abf50710169dbcb8428966c3db597448b193be3
Alex Frolagin <alexey_frolagin@rambler.ru>
2013-02-26 12:29:56 +0400

    Regular changes

--- utf-8/modules/conpay/general/conpay.php
+++ utf-8/modules/conpay/general/conpay.php
@@ -1,95 +1,187 @@
 <?php
 
-class CConpay {
-	
+class CConpay
+{
 	function GetContent($arResult = array(), $custom_settings = array(), $custom_vars = array())
 	{
 		global $APPLICATION, $USER;
+		
 		static $head_script = '';
-		static $category = '';
+		static $buttons = 0;
 		
 		if (!$arResult) return false;
+		
+		// Подключаем языковой файл
 		
 		IncludeModuleLangFile(__FILE__);
 		
 		$details = array();
 		$user_details = array();
-		$section = 0; // 0: single product; 1: catalog; 2: cart;
-		$price = 0;
+		$total = 0;
 		
-		$products = array();
+		// Если массив $arResult содержит один товар, представляем его в виде массива товаров
 		
-		if ($arResult['ITEMS']) {
-			IF ($arResult['ITEMS']['AnDelCanBuy']) {
-				$section = 2;
-				$products = $arResult['ITEMS']['AnDelCanBuy'];
-			} else {
-				$section = 1;
-				$products = $arResult['ITEMS'];
+		if (!isset($arResult['ITEMS'])) $arResult = array('ITEMS' => array($arResult));
+		
+		// Проходим по массиву товаров и создаем массив товаров для кнопки
+		
+		foreach ($arResult['ITEMS'] as $item)
+		{
+			$price 			= 0;		// Цена товара
+			$quantity 	= 1;		// Количество товара
+			$category 	= '';		// Категория товара
+			$url				= '';		// URL страницы товара
+			$image			= '';		// URL изображения товара
+			
+			$host = 'http://' . $_SERVER['HTTP_HOST'];
+			
+			// Определяем цену товара
+			
+			if (!empty($item['PRICES']['BASE']))
+			{
+				if (!empty($item['PRICES']['BASE']['DISCOUNT_VALUE']))
+				{
+					$price = (float) str_replace(' ', '', $item['PRICES']['BASE']["PRINT_DISCOUNT_VALUE_VAT"]);
+				}
+				else
+				{
+					$price = (float) str_replace(' ', '', $item['PRICES']['BASE']['PRINT_VALUE_VAT']);
+				}
 			}
-		} else {
-			$products = array($arResult);
-		}
-		
-		foreach ($products as $item) {
+			else
+			{
+				if (!empty($item["DISCOUNT_PRICE"]))
+				{
+					$price = (float) $item['DISCOUNT_PRICE'];
+				}
+				else
+				{
+					$price = (float) $item['PRICE'];
+				}
+			}
 			
-			$p = 0; $q = 1;
-			$prices = $item['PRICES']['BASE'];
+			// Определяем количество товара
 			
-			if (!$category) {
+			if (!empty($item['QUANTITY']))
+			{
+				$quantity = $item['QUANTITY'];
+			}
+			
+			// Определяем категорию товара
+			
+			if (!empty($item['SECTION']['NAME']))
+			{
+				$category = $item['SECTION']['NAME'];
+			}
+			else
+			{
 				$category = CConpay::GetCategoryNameById($item['ID']);
-				if (!$category && isset($item['IBLOCK_CODE'])) {
+				
+				if (!$category && !empty($item['IBLOCK_CODE']))
+				{
 					$category = CConpay::GetCategoryNameByCode($item['IBLOCK_CODE']);
 				}
 			}
 			
+			// Определяем URL страницы товара
+			
+			if (!empty($item['DETAIL_PAGE_URL']))
+			{
+				$url = $item['DETAIL_PAGE_URL'];
+			}
+			
+			// Bitrix 10
+			
+			elseif (!empty($item['SECTION']['SECTION_PAGE_URL']))
+			{
+				$url = $item['SECTION']['SECTION_PAGE_URL'] . $item['ID'] . '.html';
+			}
+			elseif (!empty($item['SECTION_PAGE_URL']))
+			{
+				$url = $item['SECTION_PAGE_URL'] . $item['ID'] . '.html';
+			}
+			
+			// Общий случай
+			
+			else
+			{
+				$url = 	($item['IBLOCK_TYPE_ID'] == 'catalog'? '/catalog/' : '/')
+						 . 	CConpay::GetProductNavChainByProductId($item['ID'], $item['IBLOCK_TYPE_ID'])
+						 . 	'/' . $item['ID'] . '.html';
+			}
+			
+			// Определяем URL изображения товара
+			
+			if (isset($item['DETAIL_PICTURE']) && is_array($item['DETAIL_PICTURE']) && !empty($item['DETAIL_PICTURE']['SRC']))
+			{
+				$image = $item['DETAIL_PICTURE']['SRC'];
+			}
+			else
+			{
+				$image = CConpay::GetProductImageByProductId($item['ID']);
+			}
+			
+			// Заполняем данные о товаре и добавляем его к массиву товаров
+			
 			$details[] = array(
-				'id'					=> $item['ID'],
-				'name' 				=> $item['NAME'],
-				'category' 		=> $category,
-				'url' 				=> ($host = 'http://' . $_SERVER['HTTP_HOST']) . $item['DETAIL_PAGE_URL'],
-				'image' 			=> $host . $item['DETAIL_PICTURE']['SRC'],
+				'id'					=> 	$item['ID'],
+				'name' 				=> 	$item['NAME'],
+				'category' 		=> 	$category,
+				'url' 				=> 	$host . $url,
+				'image' 			=> 	$host . $image,
 				
 				// TODO: update quantity according to user's choice
-				'quantity' 		=> ($q = (empty($item['QUANTITY']))? 1: $item['QUANTITY']),
+				
+				'quantity' 		=> 	$quantity,
+				
 				// TODO: учесть валюту цены
-				'price' 			=>	($p = ($section == 2)?
-													(float) str_replace(' ', '', $item["PRICE_FORMATED"]) :
-													(($prices["DISCOUNT_VALUE"])? (float) str_replace(' ', '', $prices["PRINT_DISCOUNT_VALUE_VAT"]) : (float) str_replace(' ', '', $prices['PRINT_VALUE_VAT']))),
+				
+				'price' 			=>	$price,
 			);
 			
-			$price += $q * $p;
+			$total += $quantity * $price;
 		}
 		
-		if ($price < COption::GetOptionString('conpay', 'MIN_PRICE', 3000)) return false;
+		if ($total < COption::GetOptionString('conpay', 'MIN_PRICE', 3000) || $total > 200000) return false;
 		
-		if ($user_id = $USER->GetID()) {
+		// Заполняем данные о пользователе
+		
+		if ($user_id = $USER->GetID())
+		{
 			$user_details['user_id'] = $user_id;
+			
 			if ($v = $USER->GetEmail())				$user_details['user_email'] 			= $v;
 			if ($v = $USER->GetFirstName())		$user_details['user_name'] 				= $v;
 			if ($v = $USER->GetLastName())		$user_details['user_lastname'] 		= $v;
 		}
 		
+		// Вызываем настройки кнопки
+		
 		$settings = array(
-			'button_class_name' 	=> ($mp = COption::GetOptionString('conpay', 'BUTTON_CLASS_NAME'))? $mp : 'conpay-btn',
-			'button_tag_name' 		=> ($mp = COption::GetOptionString('conpay', 'BUTTON_TAG_NAME'))? $mp : 'a',
-			'button_text' 				=> ($mp = COption::GetOptionString('conpay', 'BUTTON_TEXT'))? $mp : GetMessage('CONPAY_CREDIT_PURCHASE'),
-			'button_container_id' => COption::GetOptionString('conpay', 'BUTTON_CONTAINER_ID'),
+			'button_class_name' 	=> ($mp = COption::GetOptionString('conpay', 'BUTTON_CLASS_NAME'))? 	$mp : 'conpay-btn',
+			'button_tag_name' 		=> ($mp = COption::GetOptionString('conpay', 'BUTTON_TAG_NAME'))? 		$mp : 'a',
+			'button_text' 				=> ($mp = COption::GetOptionString('conpay', 'BUTTON_TEXT'))? 				$mp : GetMessage('CONPAY_CREDIT_PURCHASE'),
+			'button_container_id' => 				COption::GetOptionString('conpay', 'BUTTON_CONTAINER_ID'),
 		);
 		
-		$settings = array_merge($settings, $custom_settings);
-		$custom_vars = array_merge($user_details, $custom_vars);
+		// Уточняем настройки модуля индивидуальными настройками
 		
-		if (!$head_script) {
-			$head_script = '<script type="text/javascript" src="http://www.conpay.ru/public/api/btn.1.6.min.js"></script>';
+		$settings 		= array_merge($settings, $custom_settings);
+		$custom_vars 	= array_merge($user_details, $custom_vars);
+		
+		// Выводим функцию conpay.init, если она еще не выведена
+		
+		if (!$head_script)
+		{
+			$head_script 	= '<script type="text/javascript" src="http://www.conpay.ru/public/api/btn.1.6.min.js"></script>';
 			$head_script .= "
 			<script type=\"text/javascript\">
 				try {
 					window.conpay.init('/bitrix/modules/conpay/conpay-proxy.php', {" .
-						"'className': '" . $settings['button_class_name'] . "', " .
-						"'tagName': '" . $settings['button_tag_name'] . "', " .
-						"'text': '" . $settings['button_text'] . "'}" .
-						(($custom_vars)? ', ' . json_encode($custom_vars) : '') .
+						"'className': '" 	. $settings['button_class_name'] 	. "', " .
+						"'tagName': '" 		. $settings['button_tag_name'] 		. "', " .
+						"'text': '" 			. $settings['button_text'] 				. "'}" .
+						(($custom_vars)? ', ' . preg_replace('/^\[|,\]$/sm', '', CConpay::ConpayJsonEncode(array($custom_vars))) : '') .
 					");
 				} catch(e){};
 			</script>";
@@ -97,27 +189,74 @@ class CConpay {
 			echo $head_script;
 		}
 		
-		$script .= "
+		// Выводим функцию conpay.addButton
+		
+		$script = "
 		<script type=\"text/javascript\">
 			try {
-				window.conpay.addButton(" . json_encode($details) . (($s = $settings['button_container_id'] && $section != 1)? ", '" . $s . "'" : '') . ");
+				window.conpay.addButton("
+				. CConpay::ConpayJsonEncode($details)
+				. (($s = $settings['button_container_id'])? ", '" . $s . ($buttons >= 1? '-' . $item['ID'] : '') . "'" : '')
+				. ");
 			} catch(e){};
 		</script>";
 		
 		echo $script;
 		
+		// Обновляем счетчик кнопок
+		
+		$buttons++;
+		
 		return true;
 	}
 	
-	function GetCategoryNameById($product_id = 0) {
+	function ConpayJsonEncode($data)
+	{
+		$c = count($data);
+		$output = $c > 1? '[' : '';
 		
+		foreach ($data as $o)
+		{
+			$output .= '{';
+			foreach ($o as $k => $v)
+			{
+				$output .= "'" . $k . "': '" . htmlspecialchars($v, ENT_QUOTES, 'WINDOWS-1251') . "',";
+			}
+			$output = rtrim($output, ',') . '},';
+		}
+		$output = rtrim($output, ',') . ($c > 1? ']' : '');
+		
+		return $output;
+	}
+	
+	function GetProductImageByProductId($product_id = 0)
+	{
+		global $DB;
+		
+		$sql = "
+		SELECT CONCAT('/upload/', i.subdir, '/', i.file_name) AS image
+		FROM b_file i
+		LEFT JOIN b_iblock_element p ON p.detail_picture = i.id
+		WHERE p.id = $product_id
+		";
+		
+		$res = $DB->Query($sql);
+		$res = $res->Fetch();
+		
+		if (!$res) return '';
+		
+		return $res['image'];
+	}
+	
+	function GetCategoryNameById($product_id = 0)
+	{
 		global $DB;
 		
 		$sql = "
 		SELECT s.name AS name
 		FROM b_iblock_section s
 		LEFT JOIN b_iblock_element p ON p.iblock_section_id = s.id
-		WHERE p.id = $product_id;
+		WHERE p.id = $product_id
 		";
 		
 		$res = $DB->Query($sql);
@@ -126,6 +265,31 @@ class CConpay {
 		if (!$res) return '';
 		
 		return $res['name'];
+	}
+	
+	function GetProductNavChainByProductId($product_id, $iblock_type_id)
+	{
+		global $DB;
+		
+		$sql = "
+		SELECT	(
+							SELECT GROUP_CONCAT(u.code SEPARATOR '/')
+							FROM b_iblock_section u
+							LEFT JOIN b_iblock b ON b.id = iblock_id
+							WHERE u.left_margin <= s.left_margin AND u.right_margin >= s.right_margin AND b.iblock_type_id = '$iblock_type_id'
+							ORDER BY u.left_margin
+						) AS url
+		FROM b_iblock_element p
+		LEFT JOIN b_iblock_section s ON p.iblock_section_id = s.id
+		WHERE p.id = $product_id
+		";
+		
+		$res = $DB->Query($sql);
+		$res = $res->Fetch();
+		
+		if (!$res) return '';
+		
+		return $res['url'];
 	}
 	
 	function GetCategoryNameByCode($code = '') {
--- utf-8/modules/sale/payment/conpay/payment.php
+++ utf-8/modules/sale/payment/conpay/payment.php
@@ -1,18 +1,32 @@
-<?if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();?>
-<?
+<?php if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die(); ?>
+<?php
 
-include(GetLangFileName(dirname(__FILE__)."/", "/conpay.php"));
+include(GetLangFileName(dirname(__FILE__) . "/", "/conpay.php"));
 
 $button_text 	= htmlspecialchars_decode(CSalePaySystemAction::GetParamValue("BUTTON_TEXT"));
-$order_id 		= (strlen(CSalePaySystemAction::GetParamValue("ORDER_ID")) > 0) ? CSalePaySystemAction::GetParamValue("ORDER_ID") : $GLOBALS["SALE_INPUT_PARAMS"]["ORDER"]["ID"];
-$should_pay 	= (strlen(CSalePaySystemAction::GetParamValue("SHOULD_PAY")) > 0) ? CSalePaySystemAction::GetParamValue("SHOULD_PAY") : $GLOBALS["SALE_INPUT_PARAMS"]["ORDER"]["SHOULD_PAY"];
-$currency 		= (strlen(CSalePaySystemAction::GetParamValue("CURRENCY")) > 0) ? CSalePaySystemAction::GetParamValue("CURRENCY") : $GLOBALS["SALE_INPUT_PARAMS"]["ORDER"]["CURRENCY"];
+
+$order_id 		= (strlen(CSalePaySystemAction::GetParamValue("ORDER_ID")) 		> 0) ? CSalePaySystemAction::GetParamValue("ORDER_ID") 		: $GLOBALS["SALE_INPUT_PARAMS"]["ORDER"]["ID"];
+$should_pay 	= (strlen(CSalePaySystemAction::GetParamValue("SHOULD_PAY")) 	> 0) ? CSalePaySystemAction::GetParamValue("SHOULD_PAY") 	: $GLOBALS["SALE_INPUT_PARAMS"]["ORDER"]["SHOULD_PAY"];
+$currency 		= (strlen(CSalePaySystemAction::GetParamValue("CURRENCY")) 		> 0) ? CSalePaySystemAction::GetParamValue("CURRENCY") 		: $GLOBALS["SALE_INPUT_PARAMS"]["ORDER"]["CURRENCY"];
+
+// ¬ыбираем заказанные товары из базы данных
 
 global $DB;
 
 $sql = "
-SELECT 	p.name AS name, 
-				CONCAT('/catalog/', s.code, '/', p.code) AS url, 
+SELECT	p.id AS id,
+				p.name AS name,
+				(
+					SELECT GROUP_CONCAT(u.code SEPARATOR '/')
+					FROM b_iblock_section u
+					LEFT JOIN b_iblock ibp ON ibp.id = u.iblock_id
+					WHERE u.left_margin <= s.left_margin AND u.right_margin >= s.right_margin AND ibp.id = ib.id
+					ORDER BY u.left_margin
+				) AS navchain,
+				p.code AS code,
+				s.code AS section,
+				ib.iblock_type_id AS iblock_type_id,
+				ib.detail_page_url AS detail_page_url,
 				CONCAT('/upload/', i.subdir, '/', i.file_name) AS image,
 				s.name AS category,
 				b.price AS price,
@@ -21,21 +35,55 @@ FROM b_sale_basket b
 LEFT JOIN b_iblock_element p ON b.product_id = p.id
 LEFT JOIN b_iblock_section s ON p.iblock_section_id = s.id
 LEFT JOIN b_file i ON p.detail_picture = i.id
+LEFT JOIN b_iblock ib ON ib.id = s.iblock_id
 WHERE b.order_id = $order_id;
 ";
 
 $res = $DB->Query($sql, true);
 
+// «аполн€ем массив товаров и передаем его в модуль
+
 $products = array('ITEMS' => array());
+
 while($item = $res->Fetch()) {
 	if ($item) {
+		
+		$url = '';
+		
+		// ќпредел€ем URL страницы товара
+		
+		$url_masks = array
+		(
+			'#SITE_DIR#' 			=> '',
+			'#SECTION_CODE#' 	=> $item['section'],
+			'#ELEMENT_CODE#' 	=> $item['code'],
+			'#ELEMENT_ID#'		=> $item['id']
+		);
+		
+		if ($item['detail_page_url'])
+		{
+			$url = $item['detail_page_url'];
+			
+			foreach ($url_masks as $k => $v)
+			{
+				$url = str_replace($k, $v, $url);
+			}
+		}
+		else
+		{
+			$url = ($item['iblock_type_id'] == 'catalog'? '/catalog/' : '/') . $item['navchain'] . '/' . ($item['code']? $item['code'] : $item['id']) . '.html';
+		}
+		
+		// ƒобавл€ем свойства товара к массиву
+		
 		$products['ITEMS'][] = array(
-			'NAME' => $item['name'],
-			'SECTION' => array('NAME' => $item['category']),
-			'DETAIL_PICTURE' => array('SRC' => $item['image']),
-			'DETAIL_PAGE_URL' => $item['url'],
-			'QUANTITY' => $item['quantity'],
-			'PRICES' => array('BASE' => array('PRINT_VALUE_VAT' => $item['price'])),
+			'ID' 								=> $item['id'],
+			'NAME' 							=> $item['name'],
+			'SECTION' 					=> array('NAME' => $item['category']),
+			'DETAIL_PICTURE' 		=> array('SRC' => $item['image']),
+			'DETAIL_PAGE_URL' 	=> $url,
+			'QUANTITY' 					=> $item['quantity'],
+			'PRICE' 						=> $item['price'],
 		);
 	}
 }
@@ -43,5 +91,3 @@ while($item = $res->Fetch()) {
 if (CModule::IncludeModule('conpay')) {
 	CConpay::GetContent($products, array('button_text' => $button_text), array('order_id' => $order_id));
 }
-
-?>
