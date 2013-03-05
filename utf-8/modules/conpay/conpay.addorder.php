5273677008a1e8438a2484d8f58d4c2a021c8981
Alex Frolagin <alexey_frolagin@rambler.ru>
2013-03-05 12:51:05 +0400

    Regular changes

--- utf-8/modules/conpay/conpay.addorder.php
+++ utf-8/modules/conpay/conpay.addorder.php
@@ -5,15 +5,15 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
 	require_once($_SERVER["DOCUMENT_ROOT"] . "/bitrix/modules/main/bx_root.php");
 	require_once($_SERVER["DOCUMENT_ROOT"] . "/bitrix/modules/main/include/prolog_before.php");
 	
-	$response_pass = COption::GetOptionString('conpay', 'RESPONSE_PASS', '');
-	$merchant_id = COption::GetOptionString('conpay', 'MERCHANT_ID', '');
-	$total = 0;
+	$response_pass  = COption::GetOptionString('conpay', 'RESPONSE_PASS', '');
+	$merchant_id    = COption::GetOptionString('conpay', 'MERCHANT_ID', '');
+	$total          = 0;
 	
 	if (isset($_POST['goods']))
 	{
 		foreach ((array) $_POST['goods'] as $item)
 		{
-			$total += (float) $item['price'];
+			$total += (float) ($item['price'] * $item['quantity']);
 		}
 	}
 	
@@ -42,7 +42,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
 	}
 	
 	$customer = (array) $_POST['customer'];
-	$custom = (array) $_POST['custom'];
+	$custom   = (array) $_POST['custom'];
 	
 	if ($custom['order_id']) die('Order already exists.');
 	
@@ -50,25 +50,32 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
 	
 	$user_id = $custom['user_id']? $custom['user_id'] : IntVal($USER->GetID());
 	
-	if (!$user_id) {
+	if (!$user_id)
+  {
 		$user = new CUser;
 		
-		$login = preg_replace('/@.*/s', '', $customer['Email']);
-		$res = $user->GetByLogin($login);
+		$login  = preg_replace('/@.*/s', '', $customer['Email']);
+		$res    = $user->GetByLogin($login);
 		
-		if ($res = $res->Fetch()) {
+		if ($res = $res->Fetch())
+    {
 			$user_id = $res['ID'];
-		} else {
-			$arFields = array(
-				'LOGIN' => $login,
-				'NAME' => $customer['FirstName'],
-				'LAST_NAME' => $customer['LastName'],
-				'EMAIL' => $customer['Email'],
-				'PASSWORD' => ($pass = conpay_get_random_password()),
-				'CONFIRM_PASSWORD' => $pass,
+		}
+    else
+    {
+			$arFields = array
+      (
+				'LOGIN'             => $login,
+				'NAME'              => $customer['FirstName'],
+				'LAST_NAME'         => $customer['LastName'],
+				'EMAIL'             => $customer['Email'],
+				'PASSWORD'          => ($pass = conpay_get_random_password()),
+				'CONFIRM_PASSWORD'  => $pass,
+        'PERSONAL_PHONE'    => $customer['ContactPhone'] . (isset($customer['HomePhone'])? ', ' . $customer['HomePhone'] : ''),
 			);
 			
 			$user_id = $user->Add($arFields);
+
 			// TODO: use GetMessage for texts
 			$user->SendUserInfo($user_id, SITE_ID, "Приветствуем Вас как нового пользователя нашего интернет-магазина!", true);
 		}
@@ -76,20 +83,22 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
 	
 	$conpay_id = COption::GetOptionString('conpay', 'ps_id');
 	
-	$arFields = array(
-		 "LID" => LANG,
+	$arFields = array
+  (
+		 "LID"            => LANG,
 		 "PERSON_TYPE_ID" => 1,
-		 "PAYED" => "N",
-		 "CANCELED" => "N",
-		 "STATUS_ID" => "N",
-		 "PRICE" => $total,
-		 "CURRENCY" => "RUB",
-		 "USER_ID" => $user_id,
-		 "PAY_SYSTEM_ID" => $conpay_id,
+		 "PAYED"          => "N",
+		 "CANCELED"       => "N",
+		 "STATUS_ID"      => "N",
+		 "PRICE"          => $total,
+		 "CURRENCY"       => "RUB",
+		 "USER_ID"        => $user_id,
+		 "PAY_SYSTEM_ID"  => $conpay_id,
 	);
 	
 	// add Guest ID
-	if (CModule::IncludeModule("statistic")) {
+	if (CModule::IncludeModule("statistic"))
+  {
 		$arFields["STAT_GID"] = CStatistic::GetEventParam();
 	}
 	
@@ -97,32 +106,32 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
 	
 	foreach((array) $_POST['goods'] as $i => $item)
 	{
-		$arFields = array(
-			"ORDER_ID" => $order_id,
-			"PRODUCT_ID" => $item['id'],
-			"PRICE" => $item['price'],
-			"CURRENCY" => "RUB",
-			"QUANTITY" => IntVal($item['quantity']),
-			"LID" => LANG,
-			"NAME" => $item['name'],
+		$arFields = array
+    (
+			"ORDER_ID"    => $order_id,
+			"PRODUCT_ID"  => $item['id'],
+			"PRICE"       => $item['price'],
+			"CURRENCY"    => "RUB",
+			"QUANTITY"    => IntVal($item['quantity']),
+			"LID"         => LANG,
+			"NAME"        => $item['name'],
 		);
 		
 		CSaleBasket::Add($arFields);
 	}
 }
 
-function conpay_get_random_password() {
+function conpay_get_random_password()
+{
+	$pass         = array();
+	$alphabet     = "abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUWXYZ0123456789";
+	$alphaLength  = strlen($alphabet) - 1;
 	
-	$pass = array();
-	$alphabet = "abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUWXYZ0123456789";
-	$alphaLength = strlen($alphabet) - 1;
-	
-	for ($i = 0; $i < 8; $i++) {
-			$n = rand(0, $alphaLength);
+	for ($i = 0; $i < 8; $i++)
+  {
+			$n      = rand(0, $alphaLength);
 			$pass[] = $alphabet[$n];
 	}
 	
 	return implode($pass);
 }
-
-?>
